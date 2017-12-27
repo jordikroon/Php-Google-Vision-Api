cdbb535e328a8e1adf8c4e54aef7f51f5875e693
Jordi Kroon <jordi@jordikroon.nl>
2017-12-27 22:58:21 +0100

    Update VisionRequest test

--- tests/Request/VisionRequestTest.php
+++ tests/Request/VisionRequestTest.php
@@ -5,6 +5,10 @@ namespace Vision\Tests\Hydrator\Strategy;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use Vision\Annotation\CropHintsParams;
+use Vision\Annotation\ImageContext;
+use Vision\Annotation\LatLng;
+use Vision\Annotation\LatLongRect;
 use Vision\Image;
 use Vision\Request\VisionRequest;
 use Vision\Response\AnnotateImageResponse;
@@ -15,6 +19,11 @@ class VisionRequestTest extends \PHPUnit_Framework_TestCase
      * @var Image
      */
     protected $image;
+
+    /**
+     * @var array
+     */
+    protected $features = [];
 
     public function setUp()
     {
@@ -38,7 +47,7 @@ class VisionRequestTest extends \PHPUnit_Framework_TestCase
         $method->setAccessible(true);
         $payload = $method->invokeArgs($request, []);
 
-        $this->assertEquals($this->getPermissionTestPayload(), $payload);
+        $this->assertEquals($this->getTestPayload(), $payload);
     }
 
     public function testReturnsClientException()
@@ -84,37 +93,57 @@ class VisionRequestTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($request->getRawResponse(), $json);
     }
 
+    public function testHydratesImageContext()
+    {
+        $request = $this->getVisionRequest();
+        $class = new \ReflectionClass($request);
+        $method = $class->getMethod('extractImageContext');
+        $method->setAccessible(true);
+
+        $payload = $method->invokeArgs($request, []);
+
+        $imageContextTestPayload = $this->getTestPayload()['requests'][0]['imageContext'];
+
+        $this->assertEquals($payload, $imageContextTestPayload);
+    }
 
     /**
      * @return VisionRequest
      */
     protected function getVisionRequest()
     {
-        return new VisionRequest('abc-123', $this->image, $this->features);
+        $imageContext = new ImageContext();
+        $imageContext->setLanguageHints(['lookbehind']);
+        $imageContext->setCropHintsParams(new CropHintsParams([1.27]));
+        $imageContext->setLatLongRect(new LatLongRect(
+            new LatLng(5.3814712, 51.4455368),
+            new LatLng(51.5285582, -0.2416796)
+        ));
+
+        return new VisionRequest('abc-123', $this->image, $this->features, $imageContext);
     }
 
     /**
      * @return array
      */
-    protected function getPermissionTestPayload()
+    protected function getTestPayload()
     {
         return [
             'requests' => [
                 [
-                    'image' => [
-                        'content' => 'dGVzdA==',
-                    ],
+                    'image' => ['content' => 'dGVzdA=='],
                     'features' => [
-                        [
-                            'type' => 'LANDMARK_DETECTION',
-                            'maxResults' => 1
-                        ],
-                        [
-                            'type' => 'LABEL_DETECTION',
-                            'maxResults' => 1
-                        ]
+                        ['type' => 'LANDMARK_DETECTION', 'maxResults' => 1],
+                        ['type' => 'LABEL_DETECTION', 'maxResults' => 1]
                     ],
-                    'imageContext' => []
+                    'imageContext' => [
+                        'latLongRect' => [
+                            'minLatLng' => ['latitude' => 5.3814712, 'longitude' => 51.4455368],
+                            'maxLatLng' => ['latitude' => 51.5285582, 'longitude' => -0.2416796]
+                        ],
+                        'languageHints' => ['lookbehind'],
+                        'cropHintsParams' => ['aspectRatios' => [1.27]]
+                    ]
                 ],
             ],
         ];

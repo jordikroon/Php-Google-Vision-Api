b6480bc3ad4ad02c9a9a09334ede8a13cefcd5e9
Jordi Kroon <jordi@jordikroon.nl>
2018-01-13 01:10:46 +0100

    Rename client to httpClient

--- src/Vision.php
+++ src/Vision.php
@@ -41,27 +41,27 @@ class Vision
     /**
      * @var Client
      */
-    protected $client = null;
+    protected $httpClient;
 
     /**
      * @param string $apiKey
      * @param Feature[] $features
      * @param ImageContext|null $imageContext
      * @param string $version
-     * @param Client|null $client
+     * @param Client|null $httpClient
      */
     public function __construct(
         $apiKey,
         array $features = [],
         ImageContext $imageContext = null,
         $version = VisionRequest::VISION_VERSION,
-        Client $client = null
+        Client $httpClient = null
     ) {
         $this->apiKey = $apiKey;
         $this->version = $version;
         $this->setFeatures($features);
         $this->setImageContext($imageContext);
-        $this->client = $client;
+        $this->httpClient = $httpClient ?: new Client;
     }
 
     /**
@@ -75,7 +75,7 @@ class Vision
     ) {
         $this->visionRequest = new VisionRequest($this->apiKey, $image, $this->features, $this->imageContext);
         $this->visionRequest->setVersion($this->version);
-        $this->visionRequest->send($this->client);
+        $this->visionRequest->send($this->httpClient);
 
         return $this->getResponseForType($responseType);
     }
@@ -129,6 +129,22 @@ class Vision
     public function setImageContext($imageContext)
     {
         $this->imageContext = $imageContext ?: new ImageContext;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getHttpClient()
+    {
+        return $this->httpClient;
+    }
+
+    /**
+     * @param Client $httpClient
+     */
+    public function setHttpClient($httpClient)
+    {
+        $this->httpClient = $httpClient;
     }
 
     /**
